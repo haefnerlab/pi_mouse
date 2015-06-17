e27a98b96d7269913784e7c4f18f523e6c5cdeec
Richard Lange <rlange@Erato.local>
2015-06-17 16:47:06 -0400

    wrote Extract_Populations

--- Extract_Populations.m
+++ Extract_Populations.m
@@ -3,11 +3,35 @@ function pops = Extract_Populations(units)
 % function pops = Extract_Populations(units)
 %
 % returns list of arrays of units that have been recorded at the same time
-%
+% in the same animal
+% 
+% nu=length(units);
+% current_mouse_counter=units(1).mouse_counter;
+% current_series_number=units(1).series_num;
+% for i=2:nu
+%   
+% end
 
-nu=length(units);
-current_mouse_counter=units(1).mouse_counter;
-current_series_number=units(1).series_number;
-for i=2:nu
-  
+pops = {};
+
+mouse_ids = [units.mouse_counter];
+
+for mid=unique(mouse_ids)
+    mouse_subset = units([units.mouse_counter] == mid);
+    
+    series_ids = [mouse_subset.series_num];
+    
+    for sid=unique(series_ids)
+        % each == test makes a boolean array, and the .* acts as AND.
+        % So, we are FINDing all indices where mouse_counter is mid AND
+        % series_number is sid
+        mouse_series_ids = find(([units.mouse_counter] == mid) .* ([units.series_num] == sid));
+        pops = cell_append(pops, mouse_series_ids);
+    end
+end
+
+end
+
+function cell_array = cell_append(cell_array, value)
+    cell_array{length(cell_array)+1} = value;
 end

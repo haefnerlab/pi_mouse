dc6d07048d6ff6278347e0c8d4cf7c051db8bf91
ralf <ralf.haefner@gmail.com>
2015-06-17 17:01:54 +0200

    Added MatHelp, ToDo, started Extract_Pop

---
+++ Extract_Populations.m
@@ -0,0 +1,13 @@
+function pops = Extract_Populations(units)
+
+% function pops = Extract_Populations(units)
+%
+% returns list of arrays of units that have been recorded at the same time
+%
+
+nu=length(units);
+current_mouse_counter=units(1).mouse_counter;
+current_series_number=units(1).series_number;
+for i=2:nu
+  
+end

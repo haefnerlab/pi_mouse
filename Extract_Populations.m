463306aaa2453687ab82a571011002e8ff3a50a7
wrongu <richard.d.lange.13@dartmouth.edu>
2015-06-18 13:45:20 -0400

    Using struct array for populations that will hold statistics for each population

--- Extract_Populations.m
+++ Extract_Populations.m
@@ -2,11 +2,25 @@ function pops = Extract_Populations(units)
 
 % function pops = Extract_Populations(units)
 %
-% returns list of arrays of units that have been recorded at the same time
-% in the same animal
+% returns an array of 'population' structs.
+% A population is a set of neurons that have been recorded at the same time
+% in the same animal. The population struct (initially) has the following
+% fields:
+% - mouse_counter: mouse_counter of all units in this population
+% - series_number: series_number of all units in this population
+% - units: struct array of units in this population
+%
+% This is the first-step in computing population statistics. Other
+% functions (e.g. Compute_NoiseCorrelations) take a population as input and
+% add new fields to it
 % 
 
-pops = {};
+pops = struct(...
+    'mouse_counter', 0, ...
+    'series_number', 0, ...
+    'units', []);
+
+n_pops = 0;
 
 mouse_ids = [units.mouse_counter];
 
@@ -16,16 +30,17 @@ for mid=unique(mouse_ids)
     series_ids = [mouse_subset.series_num];
     
     for sid=unique(series_ids)
+        n_pops = n_pops + 1;
+        
         % each == test makes a boolean array, and the .* acts as AND.
-        % So, we are FINDing all indices where mouse_counter is mid AND
+        % So, we are getting all indices where mouse_counter is mid AND
         % series_number is sid
-        mouse_series_ids = find(([units.mouse_counter] == mid) .* ([units.series_num] == sid));
-        pops = cell_append(pops, mouse_series_ids);
+        population_indices = logical(([units.mouse_counter] == mid) .* ([units.series_num] == sid));
+        
+        pops(n_pops).mouse_counter = mid;
+        pops(n_pops).series_number = sid;
+        pops(n_pops).units = units(population_indices);
     end
 end
 
-end
-
-function cell_array = cell_append(cell_array, value)
-    cell_array{length(cell_array)+1} = value;
 end
